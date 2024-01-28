6d98d0fe7be916637e94f6bbeb7b8b6d49e460f8
Shin-sibainu <shincode0712@gmail.com>
2024-01-28 17:27:10 +0900

    mod

--- src/components/input/TextInput.test.tsx
+++ src/components/input/TextInput.test.tsx
@@ -1,19 +1,19 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import TextInput from "./TextInput";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TextInput from './TextInput';
 
-test("TextInput Component test", async () => {
+test('TextInput Component test', async () => {
   render(<TextInput />);
 
-  const inputElement = screen.getByRole("textbox");
+  const inputElement = screen.getByRole('textbox');
   expect(inputElement).toBeInTheDocument();
 });
 
-test("TextInput Event Test", async () => {
+test('TextInput Event Test Mod', async () => {
   const user = userEvent.setup();
   render(<TextInput />);
 
-  const inputElement = screen.getByRole("textbox");
-  await user.type(inputElement, "Hello World");
-  expect(screen.getByText("Hello World")).toBeInTheDocument();
+  const inputElement = screen.getByRole('textbox');
+  await user.type(inputElement, 'Hello World');
+  expect(screen.getByText('Hello World')).toBeInTheDocument();
 });
